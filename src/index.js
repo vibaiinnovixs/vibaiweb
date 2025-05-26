a62e3d62e2d619bb3ecd7b068b94f4dcb89e6117
vibaiinnovixs <vibaiinnovixs@gmail.com>
2025-05-26 18:39:07 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FaqPage from './pages/FaqPage';
+import VcapPage from './pages/VcapPage';
+import './styles/animations.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/vcap" element={<VcapPage />} />
+        {/* <Route path="/faq" element={<FaqPage />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
